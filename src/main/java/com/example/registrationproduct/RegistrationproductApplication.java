ea12c28f21b34a1173cdebf5ce25416cb3921a07
Lucas Barros <lucasbarrosez@gmail.com>
2021-01-14 11:52:58 -0300

    projeto criado

---
+++ src/main/java/com/example/registrationproduct/RegistrationproductApplication.java
@@ -0,0 +1,13 @@
+package com.example.registrationproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationproductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/registrationproduct/RegistrationproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrationproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
